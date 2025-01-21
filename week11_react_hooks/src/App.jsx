3663281ef1d53c333d33859d02e57dbf719c552b
Jay Patel <jay999x@gmail.com>
2025-01-21 14:53:02 +0530

    Third Commit

--- week11_react_hooks/src/App.jsx
+++ week11_react_hooks/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from "react";
+import useIsOnline from "./useIsonline";
 
 function usePrev(value){
 
@@ -19,11 +20,13 @@ function App(){
 
   const [state, setState]= useState(0)
   const prevCount = usePrev(state);
-  
+  const Online = useIsOnline()
+  console.log(Online) //returns true
   return <div style={{ textAlign: 'center', marginTop: '50px' }}>
     {prevCount}
     <button onClick={()=>{setState(currentCount => currentCount+1)}}>Increase Count</button>
     {state}
+   
     
 
   </div>
